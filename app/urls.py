c1b5961ee1f4ca5eaabea4348b161d31e8115ea7
arthurlanz <arthurlanznaster@gmail.com>
2026-02-24 14:52:22 -0300

    feat: adição dos modelos Editora e Categoria com serializers e views correspondentes

--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,14 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet
+from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet
 
 router = DefaultRouter()
 
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'editoras', EditoraViewSet)
 router.register(r'usuarios', UserViewSet, basename='usuarios')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
