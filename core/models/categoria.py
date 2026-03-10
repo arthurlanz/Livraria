27c5273b4de271f3819119de4f286c1d4305895b
arthurlanz <arthurlanznaster@gmail.com>
2026-03-10 14:14:28 -0300

    feat: inclusão do relacionamento de Livro com Categoria, Editora e Autor

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+
 class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+    
     def __str__(self):
-        return self.descricao
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Categorias"
--- core/models/livro.py
+++ core/models/livro.py
@@ -1,11 +1,22 @@
 from django.db import models
-
+from .categoria import Categoria
+from .editora import Editora
+from .autor import Autor
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=32, null=True, blank=True)
-    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    isbn = models.DecimalField(max_digits=13, decimal_places=0, null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
 
     def __str__(self):
-        return f'({self.id}) {self.titulo} ({self.quantidade})'
+        categoria_str = self.categoria.nome if self.categoria else 'Sem categoria'
+        editora_str = self.editora.nome if self.editora else 'Sem editora'
+        autor_str = self.autor.nome if self.autor else 'Sem autor'
+        
+        return (f"ID: {self.id} - {self.titulo} "
+                f"({self.quantidade or 0} disponíveis - R$ {self.preco:.2f} cada) "
+                f"- {categoria_str} - {editora_str} - {autor_str}")
